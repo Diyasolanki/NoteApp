2ed9dbc4b82b30c758c2541384cc81d78c6d9891
Diya Solanki <solankidiya28@gmail.com>
2025-04-23 09:11:37 +0530

    update changes

--- Frontend/notewebsite/vite.config.js
+++ Frontend/notewebsite/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // root: 'Frontend/notewebsite/src',
+  // base: './',
+  // build: {
+  //   // Adjust the relative path based on your project structure
+  //   outDir: '../../../dist'
+  // },
   plugins: [react()],
 })
